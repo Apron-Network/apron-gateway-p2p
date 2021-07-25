2d5a2f26b470d1352c42566e1ed4879a9ad748eb
kuleleyokn42 <74556186+kuleleyokn42@users.noreply.github.com>
2021-07-26 00:04:43 +0800

    Update data structure and protocol

--- internal/trans_network/topics.go
+++ internal/trans_network/topics.go
@@ -1,8 +1,8 @@
 package trans_network
 
 var (
-	BroadcastServiceChannel       = "broadcast_local_services"
-	ProxyReqStream                = "/proxy_req/1.0"
-	ProxyDataStreamFromClientSide = "/proxy_client_data"
-	ProxyDataStreamToServiceSide  = "/proxy_service_dat"
+	BroadcastServiceChannel        = "broadcast_local_services"
+	ProxyReqStream                 = "/proxy_req/1.0"
+	ProxyDataStreamFromClientSide  = "/proxy_client_data"
+	ProxyDataStreamFromServiceSide = "/proxy_service_data"
 )
