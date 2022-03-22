29f388a259ecef07f4757d8e13d5d3f369f6346b
freelyray <75999337+freelyray@users.noreply.github.com>
2022-03-21 23:56:22 -0700

    add unit test for ipfs pinata servie

---
+++ test/pinata_test.go
@@ -0,0 +1,36 @@
+package test
+
+import (
+	"os"
+	"testing"
+
+	"apron.network/gateway-p2p/internal/ipfs_agent"
+)
+
+var (
+	APIKEY    string
+	APISECRET string
+)
+
+func init() {
+	APIKEY = os.Getenv("PINATA_API_KEY")
+	APISECRET = os.Getenv("PINATA_SECRET_API_KEY")
+}
+
+func skip(t *testing.T) {
+	if APIKEY == "" || APISECRET == "" {
+		t.Skip("Skipping testing when without set api key and api secret")
+	}
+}
+
+func TestPinContent(t *testing.T) {
+	skip(t)
+
+	content := []byte("Hello, Apron!")
+	pinata := ipfs_agent.PinataService{APIKey: APIKEY, APISecret: APISECRET}
+	if cid, err := pinata.PinContent(content); err != nil {
+		t.Error(err)
+	} else {
+		t.Log(cid)
+	}
+}
