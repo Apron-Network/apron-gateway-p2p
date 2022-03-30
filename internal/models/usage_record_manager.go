cd6a4c75d0ac9dde229756352f5e652ffec87d9a
kuleleyokn42 <74556186+kuleleyokn42@users.noreply.github.com>
2022-03-30 23:56:47 +0800

    Swithc to proto ApronUsageRecord

--- internal/models/usage_record_manager.go
+++ internal/models/usage_record_manager.go
@@ -1,18 +1,19 @@
 package models
 
 import (
+	"google.golang.org/protobuf/proto"
 	"sync"
 	"time"
 )
 
 type UsageRecordManager struct {
-	records map[string]*UsageRecord
-	locks   map[string]*sync.Mutex
+	records map[string]*ApronUsageRecord
+	locks   map[string]*sync.RWMutex
 }
 
 func (m *UsageRecordManager) Init() {
-	m.records = make(map[string]*UsageRecord)
-	m.locks = make(map[string]*sync.Mutex)
+	m.records = make(map[string]*ApronUsageRecord)
+	m.locks = make(map[string]*sync.RWMutex)
 }
 
 func (m *UsageRecordManager) RecordUsageFromProxyRequest(proxyReq *ApronServiceRequest, reqDetail *RequestDetail) {
@@ -24,15 +25,14 @@ func (m *UsageRecordManager) RecordUsageFromProxyRequest(proxyReq *ApronServiceR
 		defer m.locks[recordKey].Unlock()
 		rcd.DoRecord(1, uint64(len(reqDetail.RequestBody)), 0)
 	} else {
-		m.locks[recordKey] = &sync.Mutex{}
+		m.locks[recordKey] = &sync.RWMutex{}
 		m.locks[recordKey].Lock()
 		defer m.locks[recordKey].Unlock()
 
-		currentTs := uint64(time.Now().UTC().Unix())
-		m.records[recordKey] = &UsageRecord{
-			AccountId:       userKey,
-			ServiceUuid:     proxyReq.ServiceId,
-			StartTimestamp:  currentTs,
+		m.records[recordKey] = &ApronUsageRecord{
+			UserKey:         userKey,
+			ServiceId:       proxyReq.ServiceId,
+			StartTs:         time.Now().UnixMilli(),
 			AccessCount:     1,
 			UploadTraffic:   uint64(len(reqDetail.RequestBody)),
 			DownloadTraffic: 0,
@@ -40,10 +40,18 @@ func (m *UsageRecordManager) RecordUsageFromProxyRequest(proxyReq *ApronServiceR
 	}
 }
 
-func (m *UsageRecordManager) ExportUsage(serviceId, userKey string) (string, error) {
-	return "", nil
-}
+// TODO: Add method to record data from ApronServiceData
 
-func (m *UsageRecordManager) ExportAllUsage() ([]*UsageRecord, error) {
-	return nil, nil
+func (m *UsageRecordManager) ExportAllUsage(nodeId string) (NodeReport, error) {
+	result := NodeReport{
+		NodeId: nodeId,
+	}
+	for recordKey, record := range m.records {
+		func() {
+			m.locks[recordKey].Lock()
+			defer m.locks[recordKey].Unlock()
+			result.Records = append(result.Records, proto.Clone(record).(*ApronUsageRecord))
+		}()
+	}
+	return result, nil
 }
