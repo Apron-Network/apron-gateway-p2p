00b8915aabbf7ec20eb4b99d2c5fc6d4912c1dfd
kuleleyokn42 <74556186+kuleleyokn42@users.noreply.github.com>
2023-01-29 01:45:55 +0800

    A new helper function for registering service to SSGW

--- internal/utils.go
+++ internal/utils.go
@@ -2,6 +2,10 @@ package internal
 
 import (
 	"bytes"
+	"encoding/json"
+
+	"apron.network/gateway-p2p/internal/models"
+	"github.com/valyala/fasthttp"
 )
 
 func CheckError(e error) {
@@ -12,4 +16,31 @@ func CheckError(e error) {
 
 func ServiceHostnameToIdByte(hostname []byte) []byte {
 	return bytes.Replace(hostname, []byte("."), []byte("_"), -1)
+}
+
+// RegisterServiceToSSGW is a helper function to register service defined by passed
+// in `ApronService` struct to given SSGW address
+func RegisterServiceToSSGW(ssgwMgmtAddr string, serviceData models.ApronService) ([]byte, error) {
+	req := fasthttp.AcquireRequest()
+	defer fasthttp.ReleaseRequest(req)
+	resp := fasthttp.AcquireResponse()
+	defer fasthttp.ReleaseResponse(resp)
+
+	newServiceReqBytes, err := json.Marshal(serviceData)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.SetRequestURI(ssgwMgmtAddr)
+	req.Header.SetMethod("POST")
+	req.SetBody(newServiceReqBytes)
+
+	err = fasthttp.Do(req, resp)
+	if err != nil {
+		return nil, err
+	}
+	CheckError(err)
+
+	respBytes := resp.Body()
+	return respBytes, nil
 }
