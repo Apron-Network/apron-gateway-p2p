0a7d9e0a9acfd1de1db1b7a738dcd72d7e5ad53a
kuleleyokn42 <74556186+kuleleyokn42@users.noreply.github.com>
2023-03-13 15:53:34 +0800

    Restructure socket_helper logger message

--- cmd/socket_helper/main.go
+++ cmd/socket_helper/main.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"apron.network/gateway-p2p/internal"
+	"apron.network/gateway-p2p/internal/logger"
 	"apron.network/gateway-p2p/internal/models"
 	"apron.network/gateway-p2p/internal/trans_network"
 	"github.com/valyala/fasthttp"
@@ -16,38 +17,44 @@ import (
 	"google.golang.org/protobuf/proto"
 )
 
-var logger *zap.SugaredLogger
-
 func main() {
-	logger = zap.NewExample().Sugar()
-	subCommamd := os.Args[1]
-	logger.Infof("sub command: %s", subCommamd)
+	subCommand := os.Args[1]
 
-	switch subCommamd {
+	switch subCommand {
 	case "client":
-		logger.Infof("client side agent")
+		logger.GetLogger().Info("command options",
+			zap.String(trans_network.EntityFieldName, trans_network.EntityCA),
+			zap.String("cli_opts", subCommand))
 		clientOpts := flag.NewFlagSet("client", flag.ExitOnError)
 		csgwAddr := clientOpts.String("csgw-socket-addr", "", "Client side GW socket address")
 		serviceId := clientOpts.String("service-id", "", "Service ID to be connected")
-		clientOpts.Parse(os.Args[2:])
+		err := clientOpts.Parse(os.Args[2:])
+		internal.CheckError(err)
 
-		go startSocketClient(*csgwAddr, *serviceId)
+		go startClientSideSocketAgent(*csgwAddr, *serviceId)
 	case "server":
-		logger.Infof("service side agent")
+		logger.GetLogger().Info("command options",
+			zap.String(trans_network.EntityFieldName, trans_network.EntitySA),
+			zap.String("cli_opts", subCommand))
 		serviceOpts := flag.NewFlagSet("service", flag.ExitOnError)
 		serviceId := serviceOpts.String("service-id", "", "id of the service")
 		listenAddr := serviceOpts.String("listen-addr", "", "Client side GW socket address")
 		ssgwMgmtAddr := serviceOpts.String("ssgw-addr", "", "RESTful management API address for service side gateway")
-		serviceOpts.Parse(os.Args[2:])
+		err := serviceOpts.Parse(os.Args[2:])
+		internal.CheckError(err)
 
-		go startSocketServer(*listenAddr, *ssgwMgmtAddr, *serviceId)
+		go startServerSideSocketAgent(*listenAddr, *ssgwMgmtAddr, *serviceId)
 	}
 
 	select {}
 }
 
 // Client related code
-func startSocketClient(csgwAddr, serviceId string) {
+func startClientSideSocketAgent(csgwAddr, serviceId string) {
+	logger.GetLogger().Debug("connect to CSGW",
+		zap.String(trans_network.EntityFieldName, trans_network.EntityCA),
+		zap.String("csgw_addr", csgwAddr))
+
 	conn, err := net.Dial("tcp", csgwAddr)
 	internal.CheckError(err)
 
@@ -55,6 +62,12 @@ func startSocketClient(csgwAddr, serviceId string) {
 	socketInitRequest := models.ApronSocketInitRequest{ServiceId: serviceId}
 	requestBytes, _ := proto.Marshal(&socketInitRequest)
 	trans_network.WriteBytesViaStream(conn, requestBytes)
+
+	logger.GetLogger().Debug("sent socket init request to CSGW",
+		zap.String(trans_network.EntityFieldName, trans_network.EntityCA),
+		zap.String("service_id", serviceId),
+		zap.ByteString("init_request_bytes", requestBytes))
+
 	time.Sleep(3 * time.Second)
 
 	go sendAndReceiveData(conn)
@@ -70,15 +83,26 @@ func sendAndReceiveData(conn net.Conn) {
 			dataBuf, err := trans_network.ReadOneFrameDataFromStream(reader)
 			internal.CheckError(err)
 
-			logger.Infof("Client: Got response: %q", dataBuf)
+			logger.GetLogger().Info("got data from client",
+				zap.String(trans_network.EntityFieldName, trans_network.EntityCA),
+				zap.Int("size", len(dataBuf)))
+			logger.GetLogger().Debug("got data from client",
+				zap.String(trans_network.EntityFieldName, trans_network.EntityCA),
+				zap.ByteString("content", dataBuf))
 		}
 	}(conn)
 
 	// Write data repeatedly
+	// Note: this application is used for demo so using this fake sender to emulate data sent from socket client.
 	go func(c net.Conn) {
 		for {
 			trans_network.WriteBytesViaStream(conn, requestBytes)
-			logger.Infof("Client: sent data, size: %d", len(requestBytes))
+			logger.GetLogger().Info("write data to client",
+				zap.String(trans_network.EntityFieldName, trans_network.EntityCA),
+				zap.Int("size", len(requestBytes)))
+			logger.GetLogger().Debug("got data from client",
+				zap.String(trans_network.EntityFieldName, trans_network.EntityCA),
+				zap.ByteString("content", requestBytes))
 			time.Sleep(time.Second)
 		}
 	}(conn)
@@ -86,7 +110,7 @@ func sendAndReceiveData(conn net.Conn) {
 }
 
 // Server related code
-func startSocketServer(listenAddr, ssgwMgmtAddr, serviceId string) {
+func startServerSideSocketAgent(listenAddr, ssgwMgmtAddr, serviceId string) {
 	registerSocketService(ssgwMgmtAddr, listenAddr, serviceId)
 
 	l, err := net.Listen("tcp", listenAddr)
@@ -95,12 +119,20 @@ func startSocketServer(listenAddr, ssgwMgmtAddr, serviceId string) {
 	for {
 		conn, err := l.Accept()
 		internal.CheckError(err)
-		logger.Infof("Connection established: %+v", conn)
+		logger.GetLogger().Info("connection with SSGW established",
+			zap.String(trans_network.EntityFieldName, trans_network.EntitySA),
+		)
 		go echoRequestData(conn)
 	}
 }
 
 func registerSocketService(ssgwAddr, listenAddr, serviceId string) {
+	logger.GetLogger().Info("prepare to register service to SSGW",
+		zap.String(trans_network.EntityFieldName, trans_network.EntitySA),
+		zap.String("ssgw_address", ssgwAddr),
+		zap.String("listen_address", listenAddr),
+		zap.String("service_id", serviceId),
+	)
 	req := fasthttp.AcquireRequest()
 	defer fasthttp.ReleaseRequest(req)
 	resp := fasthttp.AcquireResponse()
@@ -117,6 +149,11 @@ func registerSocketService(ssgwAddr, listenAddr, serviceId string) {
 		}},
 	}
 
+	logger.GetLogger().Debug("new service request",
+		zap.String(trans_network.EntityFieldName, trans_network.EntitySA),
+		zap.Any("new_service_request", newServiceRequest),
+	)
+
 	newServiceReqBytes, err := json.Marshal(newServiceRequest)
 	internal.CheckError(err)
 
@@ -128,7 +165,10 @@ func registerSocketService(ssgwAddr, listenAddr, serviceId string) {
 	internal.CheckError(err)
 
 	respBytes := resp.Body()
-	logger.Infof("Resp: %q", respBytes)
+	logger.GetLogger().Debug("response from SSGW",
+		zap.String(trans_network.EntityFieldName, trans_network.EntitySA),
+		zap.ByteString("ssgw_response", respBytes),
+	)
 }
 
 func echoRequestData(conn net.Conn) {
@@ -136,10 +176,16 @@ func echoRequestData(conn net.Conn) {
 		reader := bufio.NewReader(conn)
 		dataBuf, err := trans_network.ReadOneFrameDataFromStream(reader)
 		internal.CheckError(err)
-		logger.Infof("Server: Read %d bytes message, detail: %q", len(dataBuf), dataBuf)
+		logger.GetLogger().Info("received message",
+			zap.String(trans_network.EntityFieldName, trans_network.EntitySA),
+			zap.Int("msg_len", len(dataBuf)),
+			zap.ByteString("content", dataBuf))
 
 		writeCnt, err := conn.Write(dataBuf)
 		internal.CheckError(err)
-		logger.Infof("Server: Write %d bytes message", writeCnt)
+		logger.GetLogger().Info("message sent back",
+			zap.String(trans_network.EntityFieldName, trans_network.EntitySA),
+			zap.Int("write_cnt", writeCnt),
+			zap.ByteString("content", dataBuf))
 	}
 }
